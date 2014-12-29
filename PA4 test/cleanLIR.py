e307544783983794f0bb5162533fd264fceaff42
roeyasher <roey.asher@gmail.com>
2014-12-29 14:25:38 +0200

    py update

---
+++ PA4 test/cleanLIR.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import time
+from glob import glob
+from subprocess import *
+
+
+
+ProgFileList = glob("input/*.lir")
+for Progfile in ProgFileList:
+	os.remove(Progfile)
+	
+ProgFileList = glob("output/*.txt")
+for Progfile in ProgFileList:
+	os.remove(Progfile)
+	
+
+
